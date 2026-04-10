ff172e6819e708d2eaa63700778ad8e63b30c673
Senophyx <lyquidpersonal@gmail.com>
2026-04-10 19:21:31 +0700

    feat: add `__init__`

---
+++ pypens/__init__.py
@@ -0,0 +1,8 @@
+from .core import API
+from exceptions import APIError
+
+__title__ = "PyPENS"
+__authors__ = "Senophyx"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021-2025 Senophyx"
+__all__ = ['API', 'APIError']
