aa8cfe6a3eedde588654d3293b33dbc3fd1c1d43
Senophyx <lyquidpersonal@gmail.com>
2026-04-10 21:09:26 +0700

    fix: fix copyright on`__init__.py`

--- pypens/__init__.py
+++ pypens/__init__.py
@@ -4,5 +4,5 @@ from .exceptions import APIError
 __title__ = "PyPENS"
 __authors__ = "Senophyx"
 __license__ = "MIT License"
-__copyright__ = "Copyright 2021-2025 Senophyx"
+__copyright__ = "Copyright 2026 Senophyx"
 __all__ = ['API', 'APIError']
